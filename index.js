cb23f6388c63b8ee0f59af67a9929c2ad60c17a5
AnastasyMeleshko <7634907@mail.ru>
2023-05-18 15:03:49 +0300

    test-00: add animation for text from frame1

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ gsap.to('#loader',{
     display: 'none',
 })
 
-// backgroud-image animation
+// Backgroud-image animation
 
 gsap.to(".piece", 2, {
     scale: 0,
@@ -53,11 +53,15 @@ gsap.to("#image", {
     backgroundImage:  'url("./img.jpg")',
 });
 
+// Logo animation
+
 gsap.to('#logo', {
     delay: 8.5,
     duration: 1,
     opacity: 1,
 })
+
+// Button animation
 
 gsap.from('#cta',{
     delay: 9,
@@ -69,6 +73,86 @@ gsap.to('#cta', {
     delay: 9,
     duration: 1,
     opacity: 1,
+    y: 0,
+    x: 0,
+})
+
+// Text animation
+
+gsap.from('#textWrap', {
+    delay: 10,
+    y: -300,
+    x: -300,
+})
+
+gsap.to('#textWrap', {
+    delay: 10,
+    opacity: 1,
+    duration: 0.1,
+    y: 0,
+    x: 0,
+})
+
+const numberOfWordsInFrameOne = 16;
+
+function showText1() {
+    let delayInterval = 0.2;
+    let startDelay = 10;
+    for (let i=1; i<numberOfWordsInFrameOne; i++) {
+        gsap.to(`#word${i}`, {
+            delay: startDelay + delayInterval*i,
+            opacity: 1,
+            duration: 1,
+            y: 0,
+            x: 0,
+        })
+    }
+}
+
+showText1();
+
+gsap.to('#textWrap', {
+    delay: 15,
+    opacity: 1,
+    duration: 1,
+    y: -250,
+    x: 400,
+})
+
+function hideText1() {
+    let startDelay = 15;
+    for (let i=1; i<numberOfWordsInFrameOne; i++) {
+        gsap.to(`#word${i}`, {
+            delay: startDelay,
+            opacity: 0,
+            duration: 0.1,
+            y: 0,
+            x: 0,
+        })
+    }
+}
+
+hideText1();
+
+gsap.from('#textWrap', {
+    delay: 16,
+    y: -300,
+    x: -300,
+})
+
+gsap.to('#textWrap', {
+    delay: 16,
+    opacity: 1,
+    duration: 0.5,
+    y: 0,
+    x: 0,
+})
+
+
+gsap.to('#frame2', {
+    delay: 16,
+    opacity: 1,
+    duration: 1,
     y: 0,
     x: 0,
 })
