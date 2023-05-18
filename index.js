933c34af740212ba5d48414337a88f55d673551f
AnastasyMeleshko <7634907@mail.ru>
2023-05-19 00:09:51 +0300

    test-00: add master timeline for all animations

--- index.js
+++ index.js
@@ -8,177 +8,125 @@ wrapper.onclick = () => {
 
 // Preloader
 
-gsap.from('#my-text', {
-   duration: 2,
-   x: 0,
-   y: 500
-})
+function intro() {
+    const tl = gsap.timeline().from("#wrapper", {opacity: 0})
+        .fromTo('#my-text',
+            { duration: 2, x: 0, y: 500 },
+            { duration: 2, x: 0, y: 100 })
+        .fromTo('#photo',
+            { delay: 1, x: -100, y: -200 },
+            { delay: 1, x: 100, y: 200, rotation: 360, duration: 1.5 });
+    return tl;
+}
 
-gsap.to('#my-text', {
-    duration: 2,
-    x: 0,
-    y: 100
-})
+// Making master timeline
 
-gsap.from('#photo',{
-    delay: 2,
-    x:-100,
-    y:-200
-})
+const master = gsap.timeline({repeat: -1});
+master.add(intro())
 
-gsap.to('#photo',{
-    delay: 2,
-    x: 100,
-    y: 200,
-    rotation: 360,
-    duration: 1.5,
-})
+    // Backgroud-image animation
 
-gsap.to('#loader',{
-    delay: 4,
-    opacity: 0,
-    duration: 1,
-    display: 'none',
-})
-
-// Backgroud-image animation
-
-gsap.to(".piece", 2, {
-    scale: 0,
-    y: 60,
-    yoyo: true,
-    repeat: 1,
-    ease: "power1.inOut",
-    delay: 0.5,
-    stagger: {
+      .add(
+        gsap.to('#loader',{
+        delay: 1,
+        opacity: 0,
+        duration: 1,
+        display: 'none'})
+      )
+      .add(
+       gsap.to(".piece", 2, {
+        scale: 0,
+        y: 60,
+        yoyo: true,
+        repeat: 1,
+        ease: "power1.inOut",
+        delay: 0.5,
+        stagger: {
         amount: 1.5,
         grid: "auto",
         from: "center"
-    }
-});
+    }}))
+       .add(
+        gsap.to("#image",{
+            backgroundImage: 'url("./img.jpg")'
+        }))
 
-gsap.to("#image", {
-    delay: 8,
-    backgroundImage:  'url("./img.jpg")',
-});
+    // Logo animation
 
-// Logo animation
-
-gsap.to('#logo', {
-    delay: 8.5,
-    duration: 1,
-    opacity: 1,
-})
+    .add(
+        gsap.to('#logo', {
+        duration: 1,
+        opacity: 1,
+        })
+      )
 
 // Button animation
 
-gsap.from('#cta',{
-    delay: 9,
-    x: 0,
-    y:-200
-})
-
-gsap.to('#cta', {
-    delay: 9,
-    duration: 1,
-    opacity: 1,
-    y: 0,
-    x: 0,
-})
+    .add(
+        gsap.fromTo('#cta',
+        { x: 0, y: -200 },
+        { duration: 1, opacity: 1, y: 0, x: 0})
+    )
 
 // Text animation
 
-gsap.from('#textWrap', {
-    delay: 10,
-    y: -200,
-    x: 0,
-})
+    .add(
+        gsap.fromTo('#textWrap',
+            { y: -200, x: 0},
+            { opacity: 1, duration: 0.5, y: 0, x: 0})
+    )
 
-gsap.to('#textWrap', {
-    delay: 10.2,
-    opacity: 1,
-    duration: 0.5,
-    y: 0,
-    x: 0,
-})
+// To do - Animation for text from frame 1
+    .add(
+        gsap.to(`#frame1`, {
+            opacity: 1,
+            duration: 2.5,
+            y: 0,
+            x: 0,
+        }))
 
-const numberOfWordsInFrameOne = 16;
+//  Hide text from frame 1
 
-function showText1() {
-    let delayInterval = 0.2;
-    let startDelay = 10.2;
-    for (let i=1; i<numberOfWordsInFrameOne; i++) {
-        gsap.to(`#word${i}`, {
-            delay: startDelay + delayInterval*i,
+    .add(gsap.to('#textWrap', {
+        opacity: 0,
+        scale: 0,
+        duration: 1,
+    }))
+    .add(
+        gsap.to(`#frame1`, {
+        opacity: 0,
+        duration: 1,
+        scale: 0,
+        y: 0,
+        x: 0,
+        }))
+
+// Show text from frame 2
+
+    .add(
+        gsap.to('#textWrap', {
+        opacity: 1,
+        scale: 1,
+        duration: 1,
+        y: 0,
+        x: 0,
+}))
+    .add(
+        gsap.to('#frame2', {
             opacity: 1,
             duration: 1,
             y: 0,
             x: 0,
-        })
-    }
-}
+        }))
 
-showText1();
+// To do animation for text from frame 2
 
-gsap.to('#textWrap', {
-    delay: 15,
-    opacity: 0,
-    scale: 0,
-    duration: 1.2,
-});
-
-function hideText1() {
-    let startDelay = 15;
-    for (let i=1; i<numberOfWordsInFrameOne; i++) {
-        gsap.to(`#word${i}`, {
-            delay: startDelay,
-            opacity: 0,
+    .add(
+        gsap.to('#frame2', {
+            color: "#fac847",
             duration: 2,
-            scale: 0,
-            y: 0,
-            x: 0,
-        })
-    }
-}
+            repeat: 2,
+            yoyo: true,
+        }))
 
-hideText1();
 
-gsap.to('#textWrap', {
-    delay: 16,
-    opacity: 1,
-    scale: 1.0,
-    duration: 1.5,
-    y: 0,
-    x: 0,
-})
-
-gsap.to('#frame2', {
-    delay: 16,
-    opacity: 1,
-    duration: 1,
-    y: 0,
-    x: 0,
-})
-
-gsap.to('#frame2', {
-    color: "#fac847",
-    duration: 2,
-    repeat: -1,
-    yoyo: true,
-});
-
-//Infinite animations for banner due to simulation of reloading the page
-
-function reloadPage() {
-    setTimeout(() => {
-        location.reload();
-    }, 15000);
-}
-
-function startReload() {
-    setInterval(() => {
-        reloadPage();
-    }, 7000);
-}
-
-startReload();
